cd0b86d90d618beb912985425387ab4fa7bb5701
chanleetx12 <chanyoullee@gmail.com>
2022-04-21 00:06:04 -0700

    Add button function to append text content into formula bar

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log('hello')
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function divide(a, b) {
+    return b === 0 ? "You can't divide by zero, you stupid fuck" : a / b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function operate(operator, a, b) {
+    switch (operator) {
+        case '+':
+            add(a, b)
+            break;
+        case '-':
+            subtract(a, b);
+            break;
+        case 'x':
+            multiply(a, b);
+            break;
+        case '/':
+            divide(a, b);
+            break;
+        default:
+            break;
+
+    }
+}
+
+const formulaBar = document.querySelector('.formula-bar');
+const buttons = document.querySelectorAll('.calculator-button');
+
+buttons.forEach(button => {
+    if (button.id === 'ac') {
+        button.addEventListener('click', clearFormulaBar)
+    } else if (button.classList.contains('number') || button.classList.contains('operator')) {
+        addAppendText(button)
+    }
+})
+
+function clearFormulaBar() {
+    formulaBar.textContent = '';
+}
+
+function addAppendText(button) {
+    button.addEventListener('click', () => {
+        formulaBar.textContent += button.textContent.trim();
+    })
+}
